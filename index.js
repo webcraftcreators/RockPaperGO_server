a468d047c573db37c706439b92a9416e4ff99abd
Rimvydas996 <rimvydasradziukynas@gmail.com>
2025-03-27 22:00:16 +0200

    tetas

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 // Paprastas API endpointas
 app.get("/", (req, res) => {
-  res.json({ message: "Serveris veikia!" });
+  res.json({ message: "Serveris veikia! Testas" });
 });
 
 // WebSocket serveris
